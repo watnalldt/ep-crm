ae36eb371164cf80c6973957ec9afe4c653703b5
watnalldt <davidaturner56@yahoo.com>
2023-11-09 19:55:47 +0000

    Refactor installed apps

--- config/settings.py
+++ config/settings.py
@@ -35,15 +35,22 @@ DEBUG = env("DEBUG")
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "core.apps.CoreConfig",
 ]
+
+
+PROJECT_APPS = [
+    "core.apps.CoreConfig",
+    "users.apps.UsersConfig",
+]
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
