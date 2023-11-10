2cdccb1689bc74c81fd991db8a08898d91d38ed2
watnalldt <davidaturner56@yahoo.com>
2023-11-10 15:09:15 +0000

    Add .coveragerc file

--- users/tests.py
+++ users/tests.py
@@ -1,45 +1,55 @@
-# tests/test_models.py
 import pytest
 from django.contrib.auth import get_user_model
-from django.utils import timezone
 
 User = get_user_model()
 
 
 @pytest.mark.django_db
 def test_create_user():
-    user = User.objects.create(
-        email="test@example.com",
-        first_name="John",
-        last_name="Doe",
-        is_staff=False,
-        is_admin=False,
-        is_active=True,
-        date_joined=timezone.now(),
-        role=User.Roles.ACCOUNT_MANAGER,
-    )
+    # Test creating a regular user
+    email = "test@example.com"
+    password = "password123"
+    user = User.objects.create_user(email=email, password=password)
 
-    assert user.email == "test@example.com"
-    assert user.first_name == "John"
-    assert user.last_name == "Doe"
+    assert user.email == email
+    assert user.check_password(password)
     assert not user.is_staff
-    assert not user.is_admin
+    assert not user.is_superuser
     assert user.is_active
-    assert user.date_joined is not None
+
+
+@pytest.mark.django_db
+def test_create_superuser():
+    # Test creating a superuser
+    email = "admin@example.com"
+    password = "adminpassword123"
+    user = User.objects.create_superuser(email=email, password=password)
+
+    assert user.email == email
+    assert user.check_password(password)
+    assert user.is_staff
+    assert user.is_superuser
+    assert user.is_active
+
+
+@pytest.mark.django_db
+def test_create_superuser_invalid():
+    # Test creating a superuser with invalid fields
+    with pytest.raises(ValueError):
+        User.objects.create_superuser(email="admin@example.com", password="adminpassword123", is_staff=False)
+
+
+@pytest.mark.django_db
+def test_user_roles():
+    # Test setting user roles
+    user = User.objects.create_user(email="test@example.com", password="password123", role=User.Roles.ACCOUNT_MANAGER)
+
     assert user.role == User.Roles.ACCOUNT_MANAGER
 
 
 @pytest.mark.django_db
 def test_user_str_representation():
-    user = User.objects.create(
-        email="test@example.com", first_name="John", last_name="Doe", role=User.Roles.CLIENT_MANAGER
-    )
+    # Test the string representation of the User model
+    user = User.objects.create_user(email="test@example.com", password="password123")
 
-    assert str(user) == "test@example.com"
-
-
-@pytest.mark.django_db
-def test_user_roles():
-    assert User.Roles.ACCOUNT_MANAGER == "ACCOUNT_MANAGER"
-    assert User.Roles.CLIENT_MANAGER == "CLIENT_MANAGER"
-    assert User.Roles.ADMIN == "ADMIN"
+    assert str(user) == user.email
