b5ae2d719b3aa1d7474d903557b04125744be374
watnalldt <davidaturner56@yahoo.com>
2023-11-15 11:35:44 +0000

    Add account manager views

---
+++ core/decorators.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.decorators import user_passes_test
+
+
+def account_manager_required(
+    function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url="/users/login/"
+):
+    """
+    Decorator for views that checks that the user logging in is an account manager,
+    redirects to the log-in page if necessary.
+    """
+    actual_decorator = user_passes_test(
+        lambda u: u.is_active and u.role == "ACCOUNT_MANAGER",
+        login_url=login_url,
+        redirect_field_name=redirect_field_name,
+    )
+    return actual_decorator(function) if function else actual_decorator
--- users/views.py
+++ users/views.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_control, never_cache
+from django.views.generic import ListView, TemplateView
 
-# Create your views here.
+from clients.models import Client
+from core.decorators import account_manager_required
+from core.views import HTMLTitleMixin
+
+
+@method_decorator([never_cache, account_manager_required], name="dispatch")
+class AccountManagerView(LoginRequiredMixin, HTMLTitleMixin, TemplateView):
+    # Returns the Account Manager's Dashboard on login.
+    model = Client
+    template_name = "account_managers/account_managers_dashboard.html"
+    html_title = "My Dashboard"
+
+
+@method_decorator([never_cache, account_manager_required], name="dispatch")
+class AccountManagerClientList(LoginRequiredMixin, HTMLTitleMixin, ListView):
+    # Lists out all their client's with a link to their respective contracts
+    model = Client
+    template_name = "account_managers/client_list.html"
+    html_title = "Client List"
+    login_url = "/users/login/"
+
+    def get_queryset(self):
+        return Client.objects.filter(account_manager=self.request.user)
