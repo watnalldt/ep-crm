340634d33cfcb68b564ded467d8236e5ab14f5f9
watnalldt <davidaturner56@yahoo.com>
2023-11-15 13:39:53 +0000

    Add users templates

--- users/urls.py
+++ users/urls.py
@@ -4,19 +4,19 @@ from . import views
 
 app_name = "users"
 
-url_patterns = [
+urlpatterns = [
     path("login/", views.login, name="login"),
     path("logout/", views.logout, name="logout"),
     # Dashboards
     path("my_account/", views.my_account, name="my_account"),
     path(
-        "account_managers_dashboard/",
+        "account_managers_dashboard",
         views.AccountManagerView.as_view(),
         name="account_managers_dashboard",
     ),
     # Client List
     path(
-        "account_managers_client_list/",
+        "account_managers_client_list",
         views.AccountManagerClientList.as_view(),
         name="client_list",
     ),
