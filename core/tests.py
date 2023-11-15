c3f313611d4a3f41e98f8b9f5449e1f02004d160
watnalldt <davidaturner56@yahoo.com>
2023-11-15 10:15:01 +0000

    Create tests for htmltitlemixin

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+# test_html_title_mixin.py
 
-# Create your tests here.
+import pytest
+
+from .views import HTMLTitleMixin
+
+
+class MockView(HTMLTitleMixin):
+    html_title = "Test Title"
+    html_title_prefix = "Prefix "
+    html_title_suffix = " Suffix"
+    html_title_required = True
+
+
+def test_generate_html_title():
+    view = MockView()
+    html_title = view.generate_html_title()
+    assert html_title == "Prefix Test Title Suffix"
+
+
+def test_generate_html_title_missing_required():
+    view = MockView()
+    view.html_title_required = False
+    html_title = view.generate_html_title()
+    assert html_title == "Prefix Test Title Suffix"
+
+
+def test_generate_html_title_missing_title():
+    view = MockView()
+    view.html_title = ""
+    with pytest.raises(ValueError, match="HTMLTitleMixin requires an html_title"):
+        view.generate_html_title()
+
+
+def test_get_context_data():
+    view = MockView()
+    context = view.get_context_data()
+    assert "html_title" in context
+    assert context["html_title"] == "Prefix Test Title Suffix"
