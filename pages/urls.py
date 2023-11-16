5b68655e6fc8d711f240b1f734170f5f6d7634aa
watnalldt <davidaturner56@yahoo.com>
2023-11-16 16:41:10 +0000

    Complete static pages outlines and tests

--- pages/urls.py
+++ pages/urls.py
@@ -11,4 +11,12 @@ urlpatterns = [
         views.SeamlessUtilitiesView.as_view(),
         name="seamless_utilities",
     ),
+    path("our-partners", views.PartnersView.as_view(), name="our_partners"),
+    path("our-services", views.OurServicesView.as_view(), name="our_services"),
+    path(
+        "complaints-policy",
+        views.ComplaintsPolicyView.as_view(),
+        name="complaints_policy",
+    ),
+    path("privacy-policy", views.PrivacyPolicyView.as_view(), name="privacy_policy"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -11,3 +11,23 @@ class HomePageView(HTMLTitleMixin, TemplateView):
 class SeamlessUtilitiesView(HTMLTitleMixin, TemplateView):
     template_name = "pages/seamless_utilities.html"
     html_title = "Energy Portfolio Seamless Utilities"
+
+
+class PartnersView(HTMLTitleMixin, TemplateView):
+    template_name = "pages/our_partners.html"
+    html_title = "Energy Portfolio Our Partners"
+
+
+class OurServicesView(HTMLTitleMixin, TemplateView):
+    template_name = "pages/our_services.html"
+    html_title = "Energy Portfolio Our Services"
+
+
+class ComplaintsPolicyView(HTMLTitleMixin, TemplateView):
+    template_name = "pages/complaints_policy.html"
+    html_title = "EP Complaints Policy"
+
+
+class PrivacyPolicyView(HTMLTitleMixin, TemplateView):
+    template_name = "pages/privacy_policy.html"
+    html_title = "EP Privacy Policy"
