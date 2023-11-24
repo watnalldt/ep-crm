36cc6aa48a1837e34311ead40f4f2590af8387a7
watnalldt <davidaturner56@yahoo.com>
2023-11-24 14:30:11 +0000

    Update meter submission

--- clients/forms.py
+++ clients/forms.py
@@ -4,10 +4,6 @@ from django import forms
 
 
 class MeterForm(forms.Form):
-    METER_CHOICES = (
-        ("SINGLE_METER", "Single meter"),
-        ("MULTIPLE_METERS", "Multiple Meters"),
-    )
     from_email = forms.EmailField(required=True)
     client_name = forms.CharField(required=True)
     site_address = forms.CharField(required=True, label="Site Name:")
@@ -15,14 +11,7 @@ class MeterForm(forms.Form):
     meter_serial_number = forms.CharField(required=True, label="Meter Serial Number")
     utility_type = forms.CharField(required=True, label="Utility Type")
     supplier = forms.CharField(required=True, label="Supplier")
-    # meter_type = forms.ChoiceField(
-    #     choices=METER_CHOICES,
-    #     label="Choose Meter Type",
-    #     widget=forms.Select(attrs={"id": "meter_type"}),
-    # )
-    meter_reading = forms.CharField(max_length=100, required=False, label="Meter Reading")
-
-
+    meter_reading = forms.CharField(required=True, label="Meter Reading")
     meter_reading_date = forms.CharField(
         widget=forms.widgets.DateTimeInput(format="%d/%m/%Y"),
         initial=datetime.date.today,
